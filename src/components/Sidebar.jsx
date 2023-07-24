b7148d823e7b814ef151917839e029e0d3e7b839
Matthew Rich Marin <matthewrichmarin@gmail.com>
2023-07-24 22:43:32 +0800

    adding scroll in sidebarCart

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ function Sidebar() {
           <IoMdArrowForward className="text-2xl" />
         </div>
       </div>
-      <div>
+      <div className="flex flex-col gap-y-2 h-[670px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b">
         {cart.map((item) => {
           return <CartItem item={item} key={item.id} />;
         })}
